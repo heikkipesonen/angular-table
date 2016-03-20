ed24336b27741fe4f95200053123bc310c52426b
Heikki Pesonen <heikki.pesonen@luotta.fi>
2016-03-20 22:57:06 +0200

    remove things

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,5 +1,3 @@
-import { config } from './index.config';
-import { runBlock } from './index.run';
 
 import {TableDirective} from './directives/table';
 import {DataTableDirective} from './directives/table';
@@ -16,9 +14,7 @@ import * as extra from './directives/table-extra';
 import data from './directives/data';
 
 
-angular.module('hTable', ['ngAnimate', 'toastr'])
-  .config(config)
-  .run(runBlock)
+angular.module('hTable', ['ngAnimate'])
 
   .directive('hTable', TableDirective)
   .directive('hTableRow', TableRowDirective)
@@ -44,16 +40,61 @@ angular.module('hTable', ['ngAnimate', 'toastr'])
     'ngInject';
 
     $scope.table = {
+      /**
+       * table row data
+       * @type {Array}
+       */
       data: data,
+
+      /**
+       * is table paged or all items displayed at once?
+       * @type {Boolean}
+       */
       paged: true,
+
+      /**
+       * if paged, how many items shown at once?
+       * @type {Number}
+       */
       itemsPerPage: 10,
+
+      /**
+       * does table have detailed view of items?
+       * rowclick opens new row underneath the original one
+       * @type {Object}
+       */
       details: {
-        template: '<h2>kissa</h2>'
+        /**
+         * details view (row clicked)
+         * row data is provided as data into template scope
+         * @type {String}
+         */
+        template: '<h2>{{data.name}}</h2>'
       },
+
+      /**
+       * columns to display
+       * @type {Array}
+       */
       columns:[
         {
+
+          /**
+           * object key to use as value
+           * @type {String}
+           */
           key: 'name',
+
+          /**
+           * label for column
+           * @type {String}
+           */
           label: 'Name',
+
+          /**
+           * classnames to add to the table cell
+           * @type {String}
+           */
           classNames: 'col-lt-md-hide'
         },
 
@@ -79,16 +120,48 @@ angular.module('hTable', ['ngAnimate', 'toastr'])
           key: 'package_price',
           label: 'Price',
           classNames: 'fit-content text-center',
+
+          /**
+           * value filter
+           * manipulate model value for display
+           * @param  {[type]} value [description]
+           * @return {[type]}       [description]
+           */
           valueFilter: (value) => {
             return (value + '').replace('.',',');
           }
         }
       ],
+
+      /**
+       * row control buttons
+       * @type {Object}
+       */
       controls: {
+
+        /**
+         * left side (before cells)
+         * @type {Array}
+         */
         left: [
           {
+
+            /**
+             * icon class to add to button
+             * @type {String}
+             */
             icon: 'ion-ios-plus-outline',
+
+            /**
+             * tbd
+             * @type {String}
+             */
             type: 'icon',
+
+            /**
+             * callback to trigger when control is pressed
+             * @return {[type]} [description]
+             */
             onclick: () => {
               console.log('fukSatan');
             }
@@ -101,6 +174,11 @@ angular.module('hTable', ['ngAnimate', 'toastr'])
             }
           }
         ],
+
+        /**
+         * the other side (after the cells)
+         * @type {Array}
+         */
         right: [
           {
             icon: 'ion-ios-plus-outline',
